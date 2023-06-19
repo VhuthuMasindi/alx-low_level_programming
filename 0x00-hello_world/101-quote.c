06f8b32e11f7f461d033fa0554d3d3b8cfa8ebbb
VhuthuMasindi <masindivhuthu18@gmail.com>
2023-06-19 09:53:13 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-include <unistd.h>
+#include <unistd.h>
 
 /**
  *main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
@@ -11,4 +11,4 @@ int main(void)
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
-			
+
